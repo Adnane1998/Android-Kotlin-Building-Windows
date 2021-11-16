05f8827e763f373ea8ab51f263dcafc3154465e1
eleves <eleves@adm-pret-07.emse2000.local>
2021-11-16 21:02:19 +0100

    commit message

---
+++ app/src/main/java/com/faircorp/RoomsActivity.kt
@@ -0,0 +1,5 @@
+package com.faircorp
+
+class RoomsActivity {
+
+}
---
+++ app/src/main/java/com/faircorp/model/RoomDto.kt
@@ -0,0 +1,6 @@
+package com.faircorp.model
+
+data class RoomDto(val roomId: Long,
+                   val roomName: String,
+                   val ct: Double?,
+                   val tt: Double?)
